c6e558b1892b6e988566ee3551deed3aff5f0160
Joel Robinson <107729257+JoelARobinson32@users.noreply.github.com>
2022-12-13 10:41:03 -0600

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,9 @@
-﻿import React from "react";
+import React from "react";
 import { Route, Switch, HashRouter } from "react-router-dom";
 
 import Layout from "./layout/Layout";
 import "./layout/Layout.css";
 
-/**
- * Defines the root application component.
- * @returns {JSX.Element}
- */
 function App() {
 document.body.style.backgroundColor = "#6D877E"
   return (
