1091d24ae8dc11474aaf79d117b043f397b9e35e
JoelRobinson32 <Joelarobinson64@gmail.com>
2022-12-13 13:57:14 -0600

    Eliminated any use of Axios, which this version of Jest doesn't recognize

--- front-end/jest-puppeteer.config.js
+++ front-end/jest-puppeteer.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
-  transformIgnorePatterns: ["node_modules/(?!axios)"],
   launch: {
     headless: (process.env.HEADLESS || "true") === "true",
     slowMo: process.env.SLO_MO || 0,
--- front-end/src/reservations/ReservationUpdate.js
+++ front-end/src/reservations/ReservationUpdate.js
@@ -1,48 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router";
-import axios from "axios";
+import React from "react";
+import ReservationForm from './ReservationForm';
 
-import { formatAsDate } from "../utils/date-time";
-import ErrorAlert from "../layout/ErrorAlert";
-
-import ReservationForm from "./ReservationForm";
-
-//const axios = require('axios');
-
-// Allows you to update existing reservations
-
-export default function ReservationUpdate() {
-  const URL = "https://reservationappbyjoelbackend.onrender.com";
-  const [resExists, setResExists] = useState(null);
-  const [error, setError] = useState(null);
-
-  const { reservation_id } = useParams();
-
-  // Find an existing reservation
-  useEffect(() => {
-    const abortController = new AbortController();
-    axios
-      .get(`${URL}/reservations/${reservation_id}`, {
-        signal: abortController.signal,
-      })
-      .then((response) =>
-        setResExists({
-          ...response.data.data,
-          reservation_date: formatAsDate(response.data.data.reservation_date),
-        })
-      )
-      .catch(setError);
-    return () => abortController.abort();
-  }, [URL, reservation_id]);
-
-  // Open reservation with edit set to true.
-  return (
-    <div>
-      <h2 className="mb-2 mt-3">Update Reservation</h2>
-      <ErrorAlert error={error} />
-      {resExists && (
-        <ReservationForm resExists={resExists} edit={true}/>
-      )}
-    </div>
-  );
+function ReservationUpdate() {
+    return (
+        <div>
+            <ReservationForm edit={"true"} />
+        </div>
+    );
 }
+
+export default ReservationUpdate;
