1a25341736f8ed784bcd819a8059d57f8697668c
JoelRobinson32 <Joelarobinson64@gmail.com>
2022-12-13 11:05:02 -0600

    .
    .

--- front-end/src/tables/TablesList.js
+++ front-end/src/tables/TablesList.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useHistory } from "react-router-dom";
 
 import { emptyTable } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
